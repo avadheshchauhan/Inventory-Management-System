c47f4d9e3a6f6202edb00903a6412ef9c874b9d7
Avadhesh <avadheshchauhan955@gmail.com>
2022-05-04 09:32:18 +0530

    Change frontend folder structure

---
+++ login-and-register-frontend/src/pages/registerpage/Registerpage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import RegisterForm from '../../components/RegisterForm/RegisterForm';
+
+const Registerpage = () => {
+  return <RegisterForm />;
+};
+
+export default Registerpage;
