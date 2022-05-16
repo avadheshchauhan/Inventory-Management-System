8e8118f539e3c370c4236aa4a6d627b6e13c46f1
piyush singh <singhpiyush6101996@gmail.com>
2022-05-16 17:43:20 +0530

    inventory page added

--- login-and-register-frontend/src/components/pages/inventory/Inventory.jsx
+++ login-and-register-frontend/src/components/pages/inventory/Inventory.jsx
@@ -1,9 +1,58 @@
-const Inventory =()=>{
-    return(
-        <div>
-            <p>Inventory Page</p>
-        </div>
-    )
-}
+import { Grid, Typography } from "@mui/material";
+
+const Inventory = () => {
+  return (
+    <div>
+      <Typography>Add Product</Typography>
+
+      <form>
+        <Grid container spacing={1}>
+          <Grid xs={12} sm={6} item>
+            <TextField
+              placeholder="Enter Product name"
+              label="Product Name"
+              variant="outlined"
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid xs={12} sm={6} item>
+            <TextField
+              type="number"
+              placeholder="Enter Quantity of product"
+              label="Quantity"
+              variant="outlined"
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid xs={12} sm={6} item>
+            <TextField
+              type="number"
+              placeholder="Enter Price of product"
+              label="Price"
+              variant="outlined"
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button type="submit" variant="contained" color="primary" fullWidth>
+              Add Product
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+      <Typography>List Of Inventory Product</Typography>
+      <table>
+        <th>Sr.No</th>
+        <th>Product Id</th>
+        <th>Item Name</th>
+        <th>Quantity</th>
+        <th>Price</th>
+      </table>
+    </div>
+  );
+};
 
 export default Inventory;
--- login-and-register-frontend/src/components/pages/registerpage/Registerpage.jsx
+++ login-and-register-frontend/src/components/pages/registerpage/Registerpage.jsx
@@ -1,96 +1,112 @@
-import { Button, TextField, Typography } from "@mui/material"
-import style from "./Registerpage.module.css"
-import { useState } from "react"
-import axios from "axios"
+import { Button, TextField, Typography } from "@mui/material";
+import style from "./Registerpage.module.css";
+import { useState } from "react";
+import axios from "axios";
 import { Outlet, useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
+  let navigate = useNavigate();
+  const [error, setError] = useState();
+  const [user, setUser] = useState({
+    fullname: "",
+    email: "",
+    password: "",
+  });
 
-    let navigate = useNavigate();
-    const[error,setError]=useState()
-    const [user, setUser] = useState({
-        fullname: '',
-        email: "",
-        password: ""
-    })
+  const onchangeHandler = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
 
-    const onchangeHandler = (e) => {
-        const { name, value } = e.target
-        setUser({
-            ...user,
-            [name]: value
+  const RegisterHandler = () => {
+    const { fullname, email, password } = user;
+    console.log(user);
+    if (email && fullname && password) {
+      axios
+        .post("http://localhost:4000/register", user)
+        .then((res) => {
+          if (res.status === 200) {
+            console.log(res.status, ".....");
+            console.log(res.data.msg);
+            navigate("/dashboard");
+          }
         })
+        .catch((err) => {
+          setError("User already exist");
+          console.log("error msg", err);
+        });
     }
+  };
+  return (
+    <>
+      <form className={style.form}>
+        <div className={style.body}>
+          <Typography variant="h3" sx={{ margin: "20px" }}>
+            {" "}
+            Register
+          </Typography>
 
-    const RegisterHandler = () => {
-        const { fullname ,email, password } = user
-        console.log(user)
-        if (email && fullname && password) {
-            axios.post("http://localhost:4000/register", user)
-                .then((res)=>{
-                    if(res.status===200){
-                        console.log(res.status,".....")
-                        console.log(res.data.msg)
-                        navigate("/dashboard")
-                    }
-                  
+          <TextField
+            id="outlined-fullname"
+            label="Full Name"
+            onChange={onchangeHandler}
+            name="fullname"
+            value={user.fullname}
+            type="text"
+            color="secondary"
+            required
+            sx={{ marginBottom: "10px" }}
+            fullWidth
+            placeholder="Enter your Full Name"
+            variant="outlined"
+          />
 
-                }).catch((err)=>{
-                    setError("User already exist")
-                    console.log("error msg",err)
-                })
-        }
-    }
-    return (
-        <>
-        <form className={style.form}>
-            <div className={style.body}>
-                <Typography variant="h3" sx={{ margin: "20px" }}> Register</Typography>
+          <TextField
+            id="outlined-emailid"
+            label="Email Id"
+            onChange={onchangeHandler}
+            name="email"
+            value={user.email}
+            type="email"
+            color="secondary"
+            required
+            sx={{ marginBottom: "10px" }}
+            fullWidth
+            placeholder="Enter your Emailid"
+            variant="outlined"
+          />
 
-                <TextField id="outlined-fullname"
-                    label="Full Name"
-                    onChange={onchangeHandler}
-                    name="fullname"
-                    value={user.fullname}
-                    type="text"
-                    color="secondary"
-                    required sx={{ marginBottom: "10px" }}
-                    fullWidth placeholder="Enter your Full Name"
-                    variant="outlined" />
+          <TextField
+            id="outlined-password"
+            label=" Password"
+            onChange={onchangeHandler}
+            name="password"
+            type="password"
+            value={user.password}
+            color="secondary"
+            required
+            sx={{ marginBottom: "10px" }}
+            fullWidth
+            placeholder="Enter your password"
+            variant="outlined"
+          />
 
-                <TextField id="outlined-emailid"
-                    label="Email Id"
-                    onChange={onchangeHandler}
-                    name="email"
-                    value={user.email}
-                    type="email"
-                    color="secondary"
-                    required
-                    sx={{ marginBottom: "10px" }}
-                    fullWidth
-                    placeholder="Enter your Emailid"
-                    variant="outlined" />
-
-                <TextField id="outlined-password"
-                    label=" Password"
-                    onChange={onchangeHandler}
-                    name="password"
-                    type="password"
-                    value={user.password}
-                    color="secondary"
-                    required
-                    sx={{ marginBottom: "10px" }}
-                    fullWidth
-                    placeholder="Enter your password"
-                    variant="outlined" />
-
-                <Button size="large" onClick={RegisterHandler} sx={{ marginTop: "20px" }} variant="contained">Register</Button>
-                <p style={{color:"red"}}>{error}</p>
-            </div>
-
-        </form>
-<Outlet/>
-</>
-    )
-}
+          <Button
+            size="large"
+            onClick={RegisterHandler}
+            sx={{ marginTop: "20px" }}
+            variant="contained"
+          >
+            Register
+          </Button>
+          <p style={{ color: "red" }}>{error}</p>
+        </div>
+      </form>
+      <Outlet />
+    </>
+  );
+};
 export default RegisterPage;
